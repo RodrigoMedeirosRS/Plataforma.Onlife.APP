4f203f732d9639398eb49d110125c6d04b41a45c
R.M.Lehnemann <rodrigo.medeiros.email@gmail.com.br>
2022-11-05 08:34:44 -0300

    feat(cadastrdo de pessoa e popups)

--- APP/RES/EDUCACAO_OnLIFE/CTRL/InterfaceSobreposta.cs
+++ APP/RES/EDUCACAO_OnLIFE/CTRL/InterfaceSobreposta.cs
@@ -5,11 +5,19 @@ namespace Onlife.CTRL
 {
 	public class InterfaceSobreposta : Control
 	{
+		private AcceptDialog CaixaDeDialogo { get; set; }
+		private ConfirmationDialog CaixaDePergunta { get; set; }
+		private FileDialog CaixaDeArquivos { get; set; }
 		public override void _Ready()
 		{
-
+			PopularNodes();
 		}
-
+		private void PopularNodes()
+		{
+			CaixaDeDialogo = GetNode<AcceptDialog>("../Popups/CaixaDeDialog");
+			CaixaDePergunta = GetNode<ConfirmationDialog>("../Popups/CaixadeConfirmacao");
+			CaixaDeArquivos = GetNode<FileDialog>("./Popups/FileDialog");
+		}
 		public void _on_ExpandirButton_pressed()
 		{
 			GetNode<AnimationPlayer>("Anim").Play("Expandir");
@@ -17,9 +25,25 @@ namespace Onlife.CTRL
 
 		public void _on_CloseButton_pressed()
 		{
-			GD.Print("Vivo");
 			GetNode<AnimationPlayer>("Anim").Play("Retrair");
-			GetNode<BarraDeBusca>("./BarraDeBusca").Exibir(false);
+		}
+		private void _on_NovaPistaViva_button_up()
+		{
+			// Replace with function body.
+		}
+		private void _on_NovoRegistro_button_up()
+		{
+			// Replace with function body.
+		}
+		private void _on_NovaCidade_button_up()
+		{
+			DispararDialogo( "Por favor, clique sobre o globo marcando a posição aproximada da cidade que você quer cadastrar.");
+		}
+
+		private void DispararDialogo(string mensagem)
+		{
+			CaixaDeDialogo.DialogText = mensagem;
+			CaixaDeDialogo.Popup_();
 		}
 	}
 }
--- APP/RES/EDUCACAO_OnLIFE/CTRL/MenuSuperior.cs
+++ APP/RES/EDUCACAO_OnLIFE/CTRL/MenuSuperior.cs
@@ -3,14 +3,13 @@ using System;
 
 public class MenuSuperior : Control
 {
-    private Node nodo_pagina;
-    public override void _Ready()
-    {
-        nodo_pagina = FindParent("Main").GetNode("Pagina");
-    }
-    public void _on_TextureRect_pressed()
-    {
-
-        nodo_pagina.Call("SetPagina", "Index");
-    }
+	private Node nodo_pagina;
+	public override void _Ready()
+	{
+		//nodo_pagina = FindParent("Main").GetNode("Pagina");
+	}
+	public void _on_TextureRect_pressed()
+	{
+		nodo_pagina.Call("SetPagina", "Index");
+	}
 }
