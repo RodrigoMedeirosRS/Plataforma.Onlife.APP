c764533f374c768c01c886ad07de320a45551d13
R.M.Lehnemann <rodrigo.medeiros.email@gmail.com.br>
2022-11-05 13:52:16 -0300

    feat(limpeza de arquivos)

--- APP/RES/EDUCACAO_OnLIFE/DTO/ColisaoDTO.cs
+++ APP/RES/EDUCACAO_OnLIFE/DTO/ColisaoDTO.cs
@@ -3,13 +3,13 @@ using Godot;
 
 namespace DTO
 {
-    public class ColisaoDTO
-    {
-        public ColisaoDTO()
-        {
-            Posicao = new Vector3(0,0,0);
-        }
-        public Vector3 Posicao { get; set; }
-        public Spatial Ponto { get; set; }
-    }
+	public class ColisaoDTO
+	{
+		public ColisaoDTO()
+		{
+			Posicao = new Vector3(0,0,0);
+		}
+		public Vector3 Posicao { get; set; }
+		public Spatial Ponto { get; set; }
+	}
 }
